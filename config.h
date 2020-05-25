e51dc5781ca979eee98d621400be763e4eb9732d
ONesID3 <humbleabdi2018@gmail.com>
2020-05-25 09:59:37 +0300

    first commit

---
+++ config.h
@@ -0,0 +1,36 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int instant = 0;
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+
+static const char *fonts[] = {
+	"JetBrains Mono Medium:size=10:antialias=true:autohint=true",
+  "Hack Nerd Font:pixelsize=10:antialias=true:autohint=true", // Powerline
+  "Symbola:pixelsize=10:antialias=true:autohint=true" // Unicode
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d8dee9", "#1d1f21" },
+	[SchemeSel] = { "#268bd2", "#444" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#87cdff", "#444" },
+	[SchemeMid] = { "#87cdff", "#444" },
+
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+/* Size of the window border */
+static const unsigned int border_width = 1;
